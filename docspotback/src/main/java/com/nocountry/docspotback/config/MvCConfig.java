32166c77c337232312715790f1163bafc2565409
Anderson <andersoncusma20@gmail.com>
2024-05-19 03:11:37 -0500

    Model View Controller Configuration

---
+++ docspotback/src/main/java/com/nocountry/docspotback/config/MvCConfig.java
@@ -0,0 +1,23 @@
+package com.nocountry.docspotback.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.PathMatchConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.util.pattern.PathPatternParser;
+
+@Configuration
+public class MvCConfig implements WebMvcConfigurer {
+
+    @Override
+    public void configurePathMatch(PathMatchConfigurer configurer) {
+        PathPatternParser patternParser = new PathPatternParser();
+        patternParser.setCaseSensitive(false);
+        configurer.setPatternParser(patternParser);
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**").allowedOrigins("http://localhost:9000");
+    }
+}
