7704b31debdf29c057a5ccaf6141282524b7acc7
Anderson <andersoncusma20@gmail.com>
2024-05-19 03:19:31 -0500

    ROLE MODEL

---
+++ docspotback/src/main/java/com/nocountry/docspotback/models/Role.java
@@ -0,0 +1,26 @@
+package com.nocountry.docspotback.models;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.UUID;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+@Entity
+@Table(name = "roles")
+public class Role extends Auditable{
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID idRole=UUID.randomUUID();
+
+    @Column(name = "name_role",length = 50,nullable = false)
+    private String nameRole;
+
+    @Column(name = "description_role",length = 120)
+    private String descriptionRole;
+
+}
