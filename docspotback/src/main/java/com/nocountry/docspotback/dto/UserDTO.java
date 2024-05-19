1e93292bbcd7826d3254c9ba78f56eb71b95ab0e
Anderson <andersoncusma20@gmail.com>
2024-05-19 03:19:20 -0500

    USER DTO

---
+++ docspotback/src/main/java/com/nocountry/docspotback/dto/UserDTO.java
@@ -0,0 +1,40 @@
+package com.nocountry.docspotback.dto;
+
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+import com.nocountry.docspotback.models.Patient;
+import com.nocountry.docspotback.models.Role;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.validator.constraints.UniqueElements;
+
+import java.util.List;
+import java.util.UUID;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserDTO {
+    private UUID idUser;
+
+    @NotNull
+    @UniqueElements
+    private String email;
+
+    @NotNull
+    private String password;
+
+    @NotNull
+    private Boolean active;
+
+    @JsonManagedReference
+    @NotNull
+    private List<RoleDTO> roles;
+
+    @JsonManagedReference
+    @NotNull
+    private List<Patient> patients;
+}
