19ed186a8b723108dbabed5767feb7e894e46bc4
Anderson <andersoncusma20@gmail.com>
2024-05-19 03:15:36 -0500

    Creating the patient repository

---
+++ docspotback/src/main/java/com/nocountry/docspotback/repositories/IPatientRepo.java
@@ -0,0 +1,8 @@
+package com.nocountry.docspotback.repositories;
+
+import com.nocountry.docspotback.models.Patient;
+
+import java.util.UUID;
+
+public interface IPatientRepo extends IGenericRepo<Patient, UUID> {
+}
