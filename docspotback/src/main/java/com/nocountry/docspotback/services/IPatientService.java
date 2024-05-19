148c81603b5852653105b950b82f5d71e5d38c39
Anderson <andersoncusma20@gmail.com>
2024-05-19 03:16:38 -0500

    Creation of PATIENT CRUDs

---
+++ docspotback/src/main/java/com/nocountry/docspotback/services/IPatientService.java
@@ -0,0 +1,8 @@
+package com.nocountry.docspotback.services;
+
+import com.nocountry.docspotback.models.Patient;
+
+import java.util.UUID;
+
+public interface IPatientService extends ICRUDService<Patient, UUID>{
+}
