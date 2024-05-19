b0863ed555bb1f7d6ce9f641ef01da0572b0a3ab
Anderson <andersoncusma20@gmail.com>
2024-05-19 03:17:00 -0500

    IMPLEMENTATION of PATIENT CRUD

---
+++ docspotback/src/main/java/com/nocountry/docspotback/services/impl/PatientServiceImpl.java
@@ -0,0 +1,22 @@
+package com.nocountry.docspotback.services.impl;
+
+import com.nocountry.docspotback.models.Patient;
+import com.nocountry.docspotback.repositories.IGenericRepo;
+import com.nocountry.docspotback.repositories.IPatientRepo;
+import com.nocountry.docspotback.services.IPatientService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.UUID;
+
+@Service
+public class PatientServiceImpl extends CRUDImpl<Patient, UUID> implements IPatientService {
+
+    @Autowired
+    private IPatientRepo repo;
+
+    @Override
+    protected IGenericRepo<Patient, UUID> getRepo() {
+        return repo;
+    }
+}
