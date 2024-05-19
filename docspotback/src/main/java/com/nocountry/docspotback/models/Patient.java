b68b306d680d77694a52aafd8323e30f8f3e763d
Anderson <andersoncusma20@gmail.com>
2024-05-19 03:18:01 -0500

    PATIENT MODEL IN RELATIONSHIP TO ROLE AND PATIENT

---
+++ docspotback/src/main/java/com/nocountry/docspotback/models/Patient.java
@@ -0,0 +1,41 @@
+package com.nocountry.docspotback.models;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.UUID;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "patients")
+public class Patient extends Auditable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    @Column(name = "id_patient")
+    private UUID idPatient=UUID.randomUUID();
+
+    @Column(name = "name_patient",length = 150,nullable = false)
+    private String namePatient;
+
+    @Column(name = "cellphone_patient",length = 10,nullable = false)
+    private String cellphonePatient;
+
+    @Column(name = "photo_patient")
+    private String photoPatient;
+
+    @Column(name = "has_social_work",nullable = false)
+    private Boolean hasSocialWork;
+
+    @Column(name = "social_work",length = 15)
+    private String socialWork;
+
+    @ManyToOne
+    private User user;
+
+}
