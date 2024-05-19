7746f26101226c9f5c856a353c967ff77956c759
Anderson <andersoncusma20@gmail.com>
2024-05-19 03:20:19 -0500

    Exception Handling

---
+++ docspotback/src/main/java/com/nocountry/docspotback/exception/ModelNotFoundException.java
@@ -0,0 +1,8 @@
+package com.nocountry.docspotback.exception;
+
+public class ModelNotFoundException extends RuntimeException{
+
+    public ModelNotFoundException(String message) {
+        super(message);
+    }
+}
