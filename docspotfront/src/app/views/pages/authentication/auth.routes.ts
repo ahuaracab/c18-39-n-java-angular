feac4793fe0f895960a94ab80271629d009ebe7d
EduRamos95 <eduramos08.30@gmail.com>
2024-05-23 18:56:02 -0500

    add: config in routes and app

--- docspotfront/src/app/views/pages/authentication/auth.routes.ts
+++ docspotfront/src/app/views/pages/authentication/auth.routes.ts
@@ -1,9 +1,14 @@
 import { LoginComponent } from "./login/login.component";
 import { Routes } from '@angular/router';
+import { RegisterComponent } from "./register/register.component";
 
 export const AUTH_ROUTES: Routes = [
     {
         path: 'login',
         component: LoginComponent,
+    },
+    {
+        path: 'register',
+        component: RegisterComponent,
     }
 ]
