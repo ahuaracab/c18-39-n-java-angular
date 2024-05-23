22f147f8807d26bac90e2878064c0bbc9b1b247e
EduRamos95 <eduramos08.30@gmail.com>
2024-05-22 23:59:03 -0500

    add: inject authService in login component

--- docspotfront/src/app/views/pages/authentication/login/login.component.ts
+++ docspotfront/src/app/views/pages/authentication/login/login.component.ts
@@ -6,16 +6,30 @@ import { Router } from '@angular/router';
 import { MatIconModule } from '@angular/material/icon';
 import { userLogin } from 'src/app/models/authentication-models/login.models';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { AuthV1Service } from 'src/app/services/service-authentication/auth-v1.service';
+import {
+  HttpClientModule,
+  HttpErrorResponse,
+  HttpResponse,
+} from '@angular/common/http';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [CommonModule, ReactiveFormsModule, MatIconModule, MatProgressSpinnerModule],
+  imports: [
+    CommonModule,
+    ReactiveFormsModule,
+    MatIconModule,
+    MatProgressSpinnerModule,
+    HttpClientModule,
+  ],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.scss'
+  styleUrl: './login.component.scss',
+  providers: [
+    AuthV1Service
+  ]
 })
 export class LoginComponent implements OnInit {
-
   public loginForm!: FormGroup;
   public showPassword: boolean = false;
   public loading: boolean = false;
@@ -24,13 +38,13 @@ export class LoginComponent implements OnInit {
   constructor(
     private fb: FormBuilder,
     private router: Router,
-  ){
-  }
+    private authService: AuthV1Service
+  ) {}
 
   ngOnInit(): void {
     this.loginForm = this.fb.group({
       email: ['', [Validators.required, Validators.email]],
-      password: ['', [Validators.required, Validators.minLength(6)]]
+      password: ['', [Validators.required, Validators.minLength(6)]],
     });
   }
 
@@ -49,21 +63,26 @@ export class LoginComponent implements OnInit {
       this.errorMessage = '';
     }
 
-    const userData:userLogin = this.loginForm.value;
+    const userData: userLogin = this.loginForm.value;
 
     console.log(`email: ${userData.email}\npassword: ${userData.password}`);
-    // enviar a servicio
 
     // animacion de carga en button
     this.loading = true;
 
-    setTimeout(()=>{
-      this.loading=false;
-    },5000);
-
-    
-    // redireccion
-
-    // error message
+    // enviar a servicio
+    this.authService.login(userData).subscribe({
+      next: (response: HttpResponse<any>) => {
+        this.loading = false;
+        // guardar data del usuario en localStorage
+        // navegar a la vista segun tipo de rol
+      },
+      error: (error: HttpErrorResponse) => {
+        this.loading = false;
+        console.log("error:",error);
+        // dependiendo del error
+        this.errorMessage = 'correo o contraseña incorrecta';
+      },
+    });
   }
 }
