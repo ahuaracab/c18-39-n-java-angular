8abf695683238bdaeee1df2ab7987ad612a58fbf
Anderson Cusma <andersoncusma20@gmail.com>
2024-05-25 03:27:15 -0500

    Comentarios

--- docspotback/src/main/java/com/nocountry/docspotback/repositories/IProfessionalRepo.java
+++ docspotback/src/main/java/com/nocountry/docspotback/repositories/IProfessionalRepo.java
@@ -1,8 +1,14 @@
 package com.nocountry.docspotback.repositories;
 
 import com.nocountry.docspotback.models.Professional;
+import org.springframework.data.jpa.repository.Query;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface IProfessionalRepo extends IGenericRepo<Professional, UUID>{
+  /*  @Query(value = "Select * FROM professionals as p" +
+            "INNER JOIN Specialty as s" +
+            "WHERE p.",nativeQuery = true)
+    List<Object[]>findAllProfesionalBySpecialty(String nameSpecialty);*/
 }
