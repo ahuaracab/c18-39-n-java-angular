3eb59c00be995867c00e2019fc6888016aa91d83
Anderson Cusma <andersoncusma20@gmail.com>
2024-05-25 02:27:10 -0500

    Corrección de nombres de ids

--- docspotback/src/main/java/com/nocountry/docspotback/repositories/IReservationRepo.java
+++ docspotback/src/main/java/com/nocountry/docspotback/repositories/IReservationRepo.java
@@ -10,10 +10,10 @@ import org.springframework.data.repository.query.Param;
 
 public interface IReservationRepo extends IGenericRepo<Reservation, UUID> {
 
-    @Query(value = "SELECT r.id, r.appointment_date, r.query_intent, p.id as patien_id , p.name_patient, p.photo_patient, p.cellphone_patient\n" +
+    @Query(value = "SELECT r.id_reservation, r.appointment_date, r.query_intent, p.id_patient, p.name_patient, p.photo_patient, p.cellphone_patient\n" +
             "FROM reservations r\n" +
-            "inner join patients p on r.patient_id = p.id\n" +
-            "inner join shifts s on r.shift_id = s.id\n" +
+            "inner join patients p on r.id_patient = p.id_patient\n" +
+            "inner join shifts s on r.id_shift = s.id_shift\n" +
             "where s.professional_id_professional = :professionalId", nativeQuery = true)
     List<Object[]> findByProfessional(@Param("professionalId") UUID professionalId);
 }
