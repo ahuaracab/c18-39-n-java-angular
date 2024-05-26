8b0886cad10dffeec61c50142c52e47d56983afe
Anderson Cusma <andersoncusma20@gmail.com>
2024-05-26 01:25:57 -0500

    autentificacion keycloak

--- docspotback/src/main/java/com/nocountry/docspotback/security/KeycloakConfig.java
+++ docspotback/src/main/java/com/nocountry/docspotback/security/KeycloakConfig.java
@@ -1,47 +1,53 @@
-package com.nocountry.docspotback.security;
+    package com.nocountry.docspotback.security;
 
-import io.jsonwebtoken.security.Keys;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.web.SecurityFilterChain;
 
-import java.security.Key;
+    import io.jsonwebtoken.security.Keys;
+    import org.springframework.beans.factory.annotation.Value;
+    import org.springframework.context.annotation.Bean;
+    import org.springframework.context.annotation.Configuration;
+    import org.springframework.security.config.Customizer;
+    import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+    import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+    import org.springframework.security.core.userdetails.UserDetailsService;
+    import org.springframework.security.web.SecurityFilterChain;
+    import org.springframework.security.web.util.matcher.RequestMatcher;
 
-@Configuration
-@EnableWebSecurity
-public class KeycloakConfig {
+    import java.security.Key;
 
-    @Value("${keycloak.realm}")
-    private String realm;
+    @Configuration
+    @EnableWebSecurity
+    public class KeycloakConfig {
 
-    @Value("${keycloak.auth-server-url}")
-    private String authServerUrl;
+        @Value("${keycloak.realm}")
+        private String realm;
 
-    @Value("${keycloak.resource}")
-    private String resource;
+        @Value("${keycloak.auth-server-url}")
+        private String authServerUrl;
 
-    @Value("${keycloak.credentials.secret}")
-    private String secret;
+        @Value("${keycloak.clientId}")
+        private String clientId;
 
-    @Bean
-    public Key key() {
-        return Keys.hmacShaKeyFor(secret.getBytes());
+        @Value("${keycloak.credentials.secret}")
+        private String secret;
+
+        @Bean
+        public Key key() {
+            return Keys.hmacShaKeyFor(secret.getBytes());
+        }
+
+        @Bean
+        public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+            http.oauth2ResourceServer((oauth2) -> oauth2.jwt(Customizer.withDefaults()));
+            http.authorizeHttpRequests(authorize -> authorize
+                    .requestMatchers("/login/**").permitAll()
+                    .requestMatchers("/tokens/users/**").permitAll()
+                    .requestMatchers("/patient/**").hasRole("PATIENT")
+                    .anyRequest().authenticated());
+            return http.build();
+        }
+
+        @Bean
+        public UserDetailsService userDetailsService() {
+            return new KeycloakUserDetailsService(realm, authServerUrl, clientId, secret);
+        }
     }
-
-    @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http.oauth2ResourceServer((oauth2) -> oauth2.jwt(Customizer.withDefaults()));
-        http.authorizeHttpRequests(authorize -> authorize.anyRequest().authenticated());
-        return http.build();
-    }
-
-    @Bean
-    public UserDetailsService userDetailsService() {
-        return new KeycloakUserDetailsService(realm, authServerUrl, resource, secret);
-    }
-}
