27ab55642c5c8a09e297979a4938ae7c76a293d5
EduRamos95 <eduramos08.30@gmail.com>
2024-05-27 23:39:13 -0500

    add: config firebase

--- docspotfront/src/app/app.config.ts
+++ docspotfront/src/app/app.config.ts
@@ -3,7 +3,24 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideAnimations } from '@angular/platform-browser/animations';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getStorage, provideStorage } from '@angular/fire/storage';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideAnimations()]
+  providers: [
+    provideRouter(routes),
+    provideAnimations(),
+    provideFirebaseApp(() =>
+      initializeApp({
+        projectId: 'no-country-medical-services',
+        appId: '1:227994471282:web:3ba2d091c346af0c5e6beb',
+        storageBucket: 'no-country-medical-services.appspot.com',
+        // locationId: 'us-central',
+        apiKey: 'AIzaSyBgEaTM5MRKNTikD_E_DxBVOXusPeB8Hjk',
+        authDomain: 'no-country-medical-services.firebaseapp.com',
+        messagingSenderId: '227994471282',
+      })
+    ),
+    provideStorage(() => getStorage()),
+  ],
 };
