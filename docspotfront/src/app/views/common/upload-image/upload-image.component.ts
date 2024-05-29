5b08f7b2e9fb48ecef734838b0d0c3b51a512d9e
EduRamos95 <eduramos08.30@gmail.com>
2024-05-29 11:31:01 -0500

    update: uploadimageComponent to Output file

--- docspotfront/src/app/views/common/upload-image/upload-image.component.ts
+++ docspotfront/src/app/views/common/upload-image/upload-image.component.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import { MatFormFieldModule } from '@angular/material/form-field';
@@ -22,33 +22,59 @@ import { MatInputModule } from '@angular/material/input';
   styleUrl: './upload-image.component.scss'
 })
 export class UploadImageComponent {
-  selectedImage: string | ArrayBuffer | null = null;
+
+  @Output() public fileOut:EventEmitter<File | null> = new EventEmitter();
+
+  public selectedImage: string | ArrayBuffer | null = null;
+  public oneFile:boolean = true;
 
   onDragOver(event: DragEvent) {
     event.preventDefault();
+    if(this.selectedImage) return; 
     event.stopPropagation();
+    console.log('Drag over event');
   }
 
   onDrop(event: DragEvent) {
     event.preventDefault();
+    if(this.selectedImage) return; 
     event.stopPropagation();
+    console.log('Drop event');
     if (event.dataTransfer && event.dataTransfer.files.length > 0) {
-      this.handleFile(event.dataTransfer.files[0]);
+      console.log('Files dropped: ', event.dataTransfer.files);
+      if(event.dataTransfer.files.length != 1){
+        this.oneFile = false;
+      } else {
+        this.oneFile = true;
+        this.handleFile(event.dataTransfer.files[0]);
+      }
     }
   }
 
   onFileSelected(event: Event) {
     const input = event.target as HTMLInputElement;
+    console.log("input: ",input);
+    console.log("input-files: ",input.files);
     if (input.files && input.files.length > 0) {
+      this.oneFile = true;
       this.handleFile(input.files[0]);
     }
   }
 
   handleFile(file: File) {
+    this.fileOut.emit(file);
     const reader = new FileReader();
     reader.onload = () => {
       this.selectedImage = reader.result;
     };
     reader.readAsDataURL(file);
+  }
+
+  cancelImageSelection(event: MouseEvent, fileInput: HTMLInputElement):void {
+    event.stopPropagation();
+    this.selectedImage = null;
+    fileInput.value = '';
+    this.oneFile = true;
+    this.fileOut.emit(null);
   }
 }
