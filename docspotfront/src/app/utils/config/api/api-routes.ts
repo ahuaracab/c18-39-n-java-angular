16b0415b118b4a03d589ff6f69d4d1403c0fc43e
EduRamos95 <eduramos08.30@gmail.com>
2024-05-29 14:04:11 -0500

    add: route to get all specialties

--- docspotfront/src/app/utils/config/api/api-routes.ts
+++ docspotfront/src/app/utils/config/api/api-routes.ts
@@ -1,4 +1,7 @@
 export class ApiRoutes {
     /* LOGIN CONTROLLER */
     public static login: string = '/login/v1/authenticate';
+
+    /* SPECIALTY CONTROLLER */
+    public static specialties: string = '/api/specialty/';
 }
