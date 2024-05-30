4847cc3972a551239429343b14430a528a5b29fb
Victor Manuel Benavente Yepez <88349543+victorbenavente@users.noreply.github.com>
2024-05-30 18:31:13 -0500

    feat: add healthcheck endpoints

--- docspotback/src/main/java/com/nocountry/docspotback/controllers/Health.java
+++ docspotback/src/main/java/com/nocountry/docspotback/controllers/Health.java
@@ -5,6 +5,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,7 +17,22 @@ import org.springframework.web.bind.annotation.RestController;
 public class Health {
 
     @GetMapping
-    public ResponseEntity<Map<String, Date>> list() {
+    public ResponseEntity<Map<String, Date>> healthcheck() {
+        Map<String, Date> resp = new HashMap<>();
+        resp.put("timestamp", new Date());
+        return ResponseEntity.ok(resp);
+    }
+
+    @GetMapping("protected")
+    public ResponseEntity<Map<String, Date>> healthcheckProtected() {
+        Map<String, Date> resp = new HashMap<>();
+        resp.put("timestamp", new Date());
+        return ResponseEntity.ok(resp);
+    }
+
+    @PreAuthorize("hasRole('ADMIN')")
+    @GetMapping("protected/admin")
+    public ResponseEntity<Map<String, Date>> healthcheckProtectedAdmin() {
         Map<String, Date> resp = new HashMap<>();
         resp.put("timestamp", new Date());
         return ResponseEntity.ok(resp);
