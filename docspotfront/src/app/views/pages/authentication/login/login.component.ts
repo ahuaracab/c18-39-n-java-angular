b69d79a729165e61e1b73745e9ba55ccb268a6ad
EduRamos95 <eduramos08.30@gmail.com>
2024-05-31 22:35:56 -0500

    update: login and register components

--- docspotfront/src/app/views/pages/authentication/login/login.component.ts
+++ docspotfront/src/app/views/pages/authentication/login/login.component.ts
@@ -12,6 +12,9 @@ import {
   HttpErrorResponse,
   HttpResponse,
 } from '@angular/common/http';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 
 @Component({
   selector: 'app-login',
@@ -20,8 +23,11 @@ import {
     CommonModule,
     ReactiveFormsModule,
     MatIconModule,
+    MatButtonModule,
     MatProgressSpinnerModule,
     HttpClientModule,
+    MatFormFieldModule,
+    MatInputModule,
   ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss',
