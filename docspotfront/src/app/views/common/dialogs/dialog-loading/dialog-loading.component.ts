3c2c8eb183c8e4d76d670957c07538ba68a69636
EduRamos95 <eduramos08.30@gmail.com>
2024-06-05 17:08:28 -0500

    add: dialog-loading component

--- docspotfront/src/app/views/common/dialogs/dialog-loading/dialog-loading.component.ts
+++ docspotfront/src/app/views/common/dialogs/dialog-loading/dialog-loading.component.ts
@@ -1,12 +1,28 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, Inject } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MAT_DIALOG_DATA, MatDialogModule, MatDialogRef } from '@angular/material/dialog';
+import { MatIconModule } from '@angular/material/icon';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { DialogDataDto } from 'src/app/models/components/common/dialog.model';
 
 @Component({
   selector: 'app-dialog-loading',
   standalone: true,
-  imports: [],
+  imports: [
+    CommonModule,
+    MatIconModule,
+    MatButtonModule,
+    MatDialogModule,
+    MatProgressSpinnerModule
+  ],
   templateUrl: './dialog-loading.component.html',
   styleUrl: './dialog-loading.component.scss'
 })
 export class DialogLoadingComponent {
 
+  constructor(
+    public dialogRef: MatDialogRef<DialogLoadingComponent>,
+    @Inject(MAT_DIALOG_DATA) public data: DialogDataDto
+  ) { }
 }
