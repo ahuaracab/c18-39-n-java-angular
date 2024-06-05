a3978a17de668210ac5ad0900e458da17885c351
EduRamos95 <eduramos08.30@gmail.com>
2024-06-05 16:25:17 -0500

    add: dialog-alert component

---
+++ docspotfront/src/app/views/common/dialogs/dialog-alert/dialog-alert.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-dialog-alert',
+  standalone: true,
+  imports: [],
+  templateUrl: './dialog-alert.component.html',
+  styleUrl: './dialog-alert.component.scss'
+})
+export class DialogAlertComponent {
+
+}
