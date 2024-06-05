bd3c77aeb7dc4333f5c2322b5e2001a644b8fe6d
EduRamos95 <eduramos08.30@gmail.com>
2024-06-05 16:25:27 -0500

    add: dialog-base component

---
+++ docspotfront/src/app/views/common/dialogs/dialog-base/dialog-base.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-dialog-base',
+  standalone: true,
+  imports: [],
+  templateUrl: './dialog-base.component.html',
+  styleUrl: './dialog-base.component.scss'
+})
+export class DialogBaseComponent {
+
+}
