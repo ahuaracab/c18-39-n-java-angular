4f8685b23865f70c76ed3f57d7cd3ddee040e732
EduRamos95 <eduramos08.30@gmail.com>
2024-06-05 16:23:56 -0500

    add: render route in environments

--- docspotfront/src/environments/environment.ts
+++ docspotfront/src/environments/environment.ts
@@ -1,5 +1,8 @@
 export const environment = {
     /* LOCAL */
     url_api: "http://localhost:8000",
-    url_keylock: "http://localhost:8080"
+    url_keylock: "http://localhost:8080",
+
+    /* RENDER */
+    url_api_render: "https://docspotback.onrender.com"
 }
