b18ca2585b8bb9c623a0bf2d8c13b848b0af873b
EduRamos95 <eduramos08.30@gmail.com>
2024-06-05 16:25:52 -0500

    add: dialog-success component

---
+++ docspotfront/src/app/views/common/dialogs/dialog-success/dialog-success.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-dialog-success',
+  standalone: true,
+  imports: [],
+  templateUrl: './dialog-success.component.html',
+  styleUrl: './dialog-success.component.scss'
+})
+export class DialogSuccessComponent {
+
+}
