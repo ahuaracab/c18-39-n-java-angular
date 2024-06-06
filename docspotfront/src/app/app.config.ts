828e638133c3fe6592c4f222526475ed33238501
EduRamos95 <eduramos08.30@gmail.com>
2024-06-06 10:52:01 -0500

    add: config

--- docspotfront/src/app/app.config.ts
+++ docspotfront/src/app/app.config.ts
@@ -3,6 +3,7 @@ import { provideRouter, withViewTransitions } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideAnimations } from '@angular/platform-browser/animations';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
 import { getStorage, provideStorage } from '@angular/fire/storage';
 import { provideClientHydration } from '@angular/platform-browser';
@@ -12,6 +13,7 @@ export const appConfig: ApplicationConfig = {
     provideRouter(routes, withViewTransitions()),
     provideClientHydration(),
     provideAnimations(),
+    provideAnimationsAsync(),
     provideFirebaseApp(() =>
       initializeApp({
         projectId: 'no-country-medical-services',
