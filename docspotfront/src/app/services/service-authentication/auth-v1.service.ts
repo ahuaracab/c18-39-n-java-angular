0ed797db86a505a199a1027bdb0b3466077c6795
Anderson Cusma <andersoncusma20@gmail.com>
2024-06-06 02:34:16 -0500

    prueba

--- docspotfront/src/app/services/service-authentication/auth-v1.service.ts
+++ docspotfront/src/app/services/service-authentication/auth-v1.service.ts
@@ -16,12 +16,12 @@ export class AuthV1Service {
     private http: HttpClient,
   ) {
     this.properties = environment;
-    this.appUrl = this.properties.url_api_render;
+    this.appUrl = this.properties.url_api;
   }
 
   public login(user: userLogin): Observable<HttpResponse<any>> {
     const ctrl: string = ApiRoutes.login;
-    return this.http.post<any>(`http://localhost:8000/api/login`, user,
+    return this.http.post<any>(`https://docspotback.onrender.com/api/login`, user,
       {observe: 'response'}
     );
   }
