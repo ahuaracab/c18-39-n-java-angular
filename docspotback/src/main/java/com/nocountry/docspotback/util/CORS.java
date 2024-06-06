7e3de8803f1097c2dc9234ab4f611db88865ae27
Anderson Cusma <andersoncusma20@gmail.com>
2024-06-06 02:09:02 -0500

    CORS *

--- docspotback/src/main/java/com/nocountry/docspotback/util/CORS.java
+++ docspotback/src/main/java/com/nocountry/docspotback/util/CORS.java
@@ -34,7 +34,7 @@ public class CORS extends CorsFilter  {
         HttpServletResponse response = (HttpServletResponse) res;
         HttpServletRequest request = (HttpServletRequest) req;
 
-        response.setHeader("Access-Control-Allow-Origin", "http://localhost:4200");
+        response.setHeader("Access-Control-Allow-Origin", "*");
         response.setHeader("Access-Control-Allow-Methods", "DELETE, GET, OPTIONS, PATCH, POST, PUT");
         response.setHeader("Access-Control-Max-Age", "3600");
         response.setHeader("Access-Control-Allow-Headers", "x-requested-with, authorization, Content-Type, Authorization, credential, X-XSRF-TOKEN");
