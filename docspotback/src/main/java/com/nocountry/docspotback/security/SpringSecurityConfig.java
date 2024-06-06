202de54cfe26c70e72e4349cc73ea99f3eb11292
Anderson Cusma <andersoncusma20@gmail.com>
2024-06-06 14:14:41 -0500

    Cambios

--- docspotback/src/main/java/com/nocountry/docspotback/security/SpringSecurityConfig.java
+++ docspotback/src/main/java/com/nocountry/docspotback/security/SpringSecurityConfig.java
@@ -15,6 +15,7 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
@@ -89,6 +90,7 @@ public class SpringSecurityConfig {
         return corsbean;
     }
 
+    @CrossOrigin(allowCredentials = "http://localhost:4200")
     public JwtAuthenticationFilter jwtAuthorizationFilter() throws Exception {
         JwtAuthenticationFilter jwtAuthenticationFilter = new JwtAuthenticationFilter(authenticationManager());
         jwtAuthenticationFilter.setFilterProcessesUrl("/api/login");
