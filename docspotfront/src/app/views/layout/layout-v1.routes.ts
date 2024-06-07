57bccb54068bfcf4fcaf1426db1f36314580d36a
EduRamos95 <eduramos08.30@gmail.com>
2024-06-07 18:09:59 -0500

    add: layout routes

---
+++ docspotfront/src/app/views/layout/layout-v1.routes.ts
@@ -0,0 +1,12 @@
+import { Routes } from '@angular/router';
+import { LayoutV1Component } from './layout-v1/layout-v1.component';
+
+export const LAYOUT_V1_ROUTES: Routes = [
+    {
+        path: 'v1',
+        component: LayoutV1Component,
+        loadChildren: () =>
+            import('src/app/views/pages/patient-home/patient-home.routes')
+              .then((m) => m.PATIENT_HOME_ROUTES),
+    }
+]
