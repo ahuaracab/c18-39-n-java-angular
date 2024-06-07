f0ca00248038b5fa4e81979b007010ab1cdc8707
EduRamos95 <eduramos08.30@gmail.com>
2024-06-07 18:08:41 -0500

    add: header component

---
+++ docspotfront/src/app/views/layout/components/header-v1/header-v1.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { HeaderV1Component } from './header-v1.component';
+
+describe('HeaderV1Component', () => {
+  let component: HeaderV1Component;
+  let fixture: ComponentFixture<HeaderV1Component>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [HeaderV1Component]
+    })
+    .compileComponents();
+    
+    fixture = TestBed.createComponent(HeaderV1Component);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
