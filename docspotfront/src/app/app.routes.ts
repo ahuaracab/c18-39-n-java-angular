4f38b1577beefb94457242ebfead36be7fe92552
FerEspinal22 <ferespinal2001@gmail.com>
2024-06-11 00:55:19 -0600

    Create layout and footer component

--- docspotfront/src/app/app.routes.ts
+++ docspotfront/src/app/app.routes.ts
@@ -1,43 +1,18 @@
 import { Routes } from '@angular/router';
 
 export const routes: Routes = [
-  { path: '', pathMatch: 'full', redirectTo: '/home' },
   {
-    path: 'auth',
-    //   canActivate: [BeforeUserLoggedGuard],
-    loadChildren: () =>
-      import('./views/pages/authentication/auth.routes').then(
-        (m) => m.AUTH_ROUTES
-      ),
+    path: '',
+    children: [
+      {
+        path: 'auth',
+        loadChildren: () => import('./views/pages/authentication/auth.routes').then(c => c.AUTH_ROUTES),
+      },
+      {
+        path: '',
+        loadChildren: () => import('./views/layout/layout.routes').then(c => c.LAYOUT_ROUTES),
+      },
+    ],
   },
-  {
-    path: 'doctors',
-    loadChildren: () =>
-      import(
-        './views/pages/search-profesional/search-professional.routes'
-      ).then((c) => c.SEARCH_PROFESSIONAL_ROUTES),
-  },
-  {
-    path: 'home',
-    loadChildren: () => import('./views/pages/home/home.routes').then((c) => c.HOME_ROUTES),
-  },
-  {
-    path: 'page-not-found',
-    loadComponent: () => import('./views/common/page-not-found/page-not-found.component').then(c => c.PageNotFoundComponent)
-  },
-  {
-    path: "app",
-    // canActivate: [AfterUserLoggedGuard],
-    loadChildren: () =>
-      import('src/app/views/layout/layout-v1/layout-v1.routes')
-        .then((m) => m.LAYOUT_V1_ROUTES),
-  },
-  {
-    path: "app",
-    // canActivate: [AfterUserLoggedGuard],
-    loadChildren: () =>
-      import('src/app/views/layout/layout-v2/layout-v2.routes')
-        .then((m) => m.LAYOUT_V2_ROUTES),
-  },
-  { path: '**', pathMatch: 'full', redirectTo: '/page-not-found' },
+
 ];
