3d02fce554016d86ddc82ced6b6de2ba9bfdf1fb
EduRamos95 <eduramos08.30@gmail.com>
2024-06-13 18:56:46 -0500

    add: models

--- docspotfront/src/app/utils/config/api/api-routes.ts
+++ docspotfront/src/app/utils/config/api/api-routes.ts
@@ -10,4 +10,10 @@ export class ApiRoutes {
 
     /* SPECIALTY CONTROLLER */
     public static specialties: string = '/api/specialty';
+
+    /* SHIFT CONTROLLER */
+    public static shifts: string = '/api/shifts/professional';
+
+    /* RESERVATIONS CONTROLLER */
+    public static reservations: string = '/api/reservations';
 }
